0b650048f4994d07dfa77b6e63bdbd0174768b09
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-01-23 00:39:20 +0100

    Fix some issues inside the user entity

--- MicroServices/UserService/Entities/User.cs
+++ MicroServices/UserService/Entities/User.cs
@@ -52,7 +52,7 @@ namespace UserService.Entities
     }
     public class UserToken
     {
-        public User User {get; set;}
-        public string Token { get; set; }
+        public User? User {get; set;}
+        public string? Token { get; set; }
     }
 }
