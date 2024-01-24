68e5deba0bbb407e665a49d02dc0967226de862e
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-01-24 05:30:57 +0100

    fix some issues

--- MicroServices/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ MicroServices/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d33caed34ef3826528d0e4fa37688281028e3c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b1c387b6e1e02f19116eb7dc7c6307f0430a701")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
