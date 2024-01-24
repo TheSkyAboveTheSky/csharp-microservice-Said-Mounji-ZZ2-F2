f4c234caa563a8b2396a2b29e42e0ce9e512b169
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-01-24 04:30:18 +0100

    add signin form

--- FrontAppBlazor/obj/Debug/net8.0/FrontAppBlazor.AssemblyInfo.cs
+++ FrontAppBlazor/obj/Debug/net8.0/FrontAppBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontAppBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6384226c23abeacf762f1d71345f8a522696dac1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7beda11ac29d9a74eed8f7063c495f1c00d6d69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontAppBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontAppBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
