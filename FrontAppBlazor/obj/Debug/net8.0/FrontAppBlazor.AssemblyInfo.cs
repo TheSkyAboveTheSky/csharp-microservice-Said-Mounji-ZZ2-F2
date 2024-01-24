7beda11ac29d9a74eed8f7063c495f1c00d6d69c
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-01-24 04:04:57 +0100

    add navbar & footer

--- FrontAppBlazor/obj/Debug/net8.0/FrontAppBlazor.AssemblyInfo.cs
+++ FrontAppBlazor/obj/Debug/net8.0/FrontAppBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontAppBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15d5fded84def28b146454b47b58acfb81540b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6384226c23abeacf762f1d71345f8a522696dac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontAppBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontAppBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MicroServices/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ MicroServices/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d33caed34ef3826528d0e4fa37688281028e3c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6384226c23abeacf762f1d71345f8a522696dac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
