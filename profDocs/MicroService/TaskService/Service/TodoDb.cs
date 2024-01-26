539d728b64527de072bbb115f2e8f988b95132ee
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-01-26 03:13:32 +0100

    all working v2

--- profDocs/MicroService/TaskService/Service/TodoDb.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TaskService.Service
-{
-    public class TodoDb
-    {
-        public Dictionary<int, List<Entities.Todo>> Todos { get; } = new Dictionary<int, List<Entities.Todo>>();
-
-    }
-}
